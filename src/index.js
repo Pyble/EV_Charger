a81924b28f5e26e7acb59d66639be3203ed26c27
Woon Jang <dj4ngb0g0h@gmail.com>
2025-02-17 18:17:49 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import evstation from './evstation';
+import evStation from './evStation';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
